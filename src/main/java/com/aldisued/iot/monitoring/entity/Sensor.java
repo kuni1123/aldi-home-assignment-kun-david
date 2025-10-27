0bec8d769edcc6a0674cc853a443e6f4f276c251
56839 <david.kun2@4ig.hu>
2025-10-27 12:43:34 +0100

    Task 2 solution

--- src/main/java/com/aldisued/iot/monitoring/entity/Sensor.java
+++ src/main/java/com/aldisued/iot/monitoring/entity/Sensor.java
@@ -1,13 +1,17 @@
 package com.aldisued.iot.monitoring.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
@@ -16,79 +20,86 @@ import java.util.UUID;
 @Entity
 public class Sensor {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.UUID)
-  private UUID id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
-  @Column(nullable = false)
-  private String name;
+    @Column(nullable = false)
+    private String name;
 
-  @Column(nullable = false)
-  @Enumerated(EnumType.STRING)
-  private SensorType type;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private SensorType type;
 
-  public Sensor() {}
+    @OneToMany(mappedBy = "sensor", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Alert> alerts;
 
-  public Sensor(String name, SensorType type) {
-    this.name = name;
-    this.type = type;
-  }
+    @OneToMany(mappedBy = "sensor", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<SensorReading> sensorReadings;
 
-  public UUID getId() {
-    return id;
-  }
-
-  public void setId(UUID id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public SensorType getType() {
-    return type;
-  }
-
-  public void setType(SensorType type) {
-    this.type = type;
-  }
-
-  public List<Alert> getAlerts() {
-    //TODO: Task 2
-    return null;
-  }
-
-  public void setAlerts(List<Alert> alerts) {
-    //TODO: Task 2
-  }
-
-  public List<SensorReading> getSensorReadings() {
-    //TODO: Task 2
-    return null;
-  }
-
-  public void setSensorReadings(
-      List<SensorReading> sensorReadings) {
-    //TODO: Task 2
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (o == null || getClass() != o.getClass()) {
-      return false;
+    public Sensor() {
     }
-    Sensor sensor = (Sensor) o;
-    return Objects.equals(id, sensor.id) && Objects.equals(name, sensor.name)
-        && type == sensor.type;
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name, type);
-  }
+    public Sensor(String name, SensorType type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public SensorType getType() {
+        return type;
+    }
+
+    public void setType(SensorType type) {
+        this.type = type;
+    }
+
+    public List<Alert> getAlerts() {
+        return this.alerts;
+    }
+
+    public void setAlerts(List<Alert> alerts) {
+        this.alerts = alerts;
+    }
+
+    public List<SensorReading> getSensorReadings() {
+        return this.sensorReadings;
+    }
+
+    public void setSensorReadings(List<SensorReading> sensorReadings) {
+        this.sensorReadings = sensorReadings;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Sensor sensor = (Sensor) o;
+        return Objects.equals(id, sensor.id)
+                && Objects.equals(name, sensor.name)
+                && type == sensor.type
+                && Objects.equals(alerts, sensor.alerts)
+                && Objects.equals(sensorReadings, sensor.sensorReadings);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, type);
+    }
 }
--- src/main/java/com/aldisued/iot/monitoring/entity/SensorReading.java
+++ src/main/java/com/aldisued/iot/monitoring/entity/SensorReading.java
@@ -2,69 +2,76 @@ package com.aldisued.iot.monitoring.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
 import java.time.LocalDateTime;
 
 @Table(name = "sensor_readings")
 @Entity
 public class SensorReading {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-  @Column(nullable = false)
-  private Double value;
+    @Column(nullable = false)
+    private Double value;
 
-  @Column(nullable = false)
-  private LocalDateTime timestamp;
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
 
-  public SensorReading() {
-  }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "SENSOR_ID")
+    private Sensor sensor;
 
-  public SensorReading(
-      Double value,
-      LocalDateTime timestamp,
-      Sensor sensor
-  ) {
-    this.value = value;
-    this.timestamp = timestamp;
-    //TODO: Task 2
-  }
+    public SensorReading() {
+    }
 
-  public Long getId() {
-    return id;
-  }
+    public SensorReading(
+            Double value,
+            LocalDateTime timestamp,
+            Sensor sensor
+    ) {
+        this.value = value;
+        this.timestamp = timestamp;
+        this.sensor = sensor;
+    }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public Double getValue() {
-    return value;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public void setValue(Double value) {
-    this.value = value;
-  }
+    public Double getValue() {
+        return value;
+    }
 
-  public LocalDateTime getTimestamp() {
-    return timestamp;
-  }
+    public void setValue(Double value) {
+        this.value = value;
+    }
 
-  public void setTimestamp(LocalDateTime timestamp) {
-    this.timestamp = timestamp;
-  }
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
 
-  public Sensor getSensor() {
-    //TODO: Task 2
-    return null;
-  }
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 
-  public void setSensor(Sensor sensor) {
-    //TODO: Task 2
-  }
+    public Sensor getSensor() {
+        return this.sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
 }
