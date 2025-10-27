409345eac0ed9ed5954c4d4545f08905a2cb84ec
56839 <david.kun2@4ig.hu>
2025-10-27 14:16:12 +0100

    Task 5 solution

--- src/main/java/com/aldisued/iot/monitoring/controller/AlertController.java
+++ src/main/java/com/aldisued/iot/monitoring/controller/AlertController.java
@@ -1,17 +1,28 @@
 package com.aldisued.iot.monitoring.controller;
 
+import com.aldisued.iot.monitoring.dto.AlertDto;
 import com.aldisued.iot.monitoring.service.AlertService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/alerts")
 public class AlertController {
 
-  private final AlertService alertService;
+    private final AlertService alertService;
 
-  public AlertController(AlertService alertService) {
-    this.alertService = alertService;
-  }
+    public AlertController(AlertService alertService) {
+        this.alertService = alertService;
+    }
+
+    @GetMapping("/latest")
+    public AlertDto findLatestAlerts(@Valid @RequestParam UUID sensorId) {
+        return this.alertService.findLastAlertBySensorId(sensorId);
+    }
 
 }
--- src/main/java/com/aldisued/iot/monitoring/service/AlertService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/AlertService.java
@@ -2,33 +2,36 @@ package com.aldisued.iot.monitoring.service;
 
 import com.aldisued.iot.monitoring.dto.AlertDto;
 import com.aldisued.iot.monitoring.entity.Alert;
+import com.aldisued.iot.monitoring.exception.EntityNotFoundException;
 import com.aldisued.iot.monitoring.repository.AlertRepository;
 import com.aldisued.iot.monitoring.repository.SensorRepository;
-import java.util.UUID;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class AlertService {
 
-  private final AlertRepository alertRepository;
-  private final SensorRepository sensorRepository;
-  private final KafkaTemplate<String, AlertDto> kafkaTemplate;
+    private final AlertRepository alertRepository;
+    private final SensorRepository sensorRepository;
+    private final KafkaTemplate<String, AlertDto> kafkaTemplate;
 
-  public AlertService(AlertRepository alertRepository, SensorRepository sensorRepository,
-      KafkaTemplate<String, AlertDto> kafkaTemplate) {
-    this.alertRepository = alertRepository;
-    this.sensorRepository = sensorRepository;
-    this.kafkaTemplate = kafkaTemplate;
-  }
+    public AlertService(AlertRepository alertRepository, SensorRepository sensorRepository,
+                        KafkaTemplate<String, AlertDto> kafkaTemplate) {
+        this.alertRepository = alertRepository;
+        this.sensorRepository = sensorRepository;
+        this.kafkaTemplate = kafkaTemplate;
+    }
 
-  public Alert saveAlert(AlertDto alertDto) {
-    // TODO: Task 6
-    return null;
-  }
+    public Alert saveAlert(AlertDto alertDto) {
+        // TODO: Task 6
+        return null;
+    }
 
-  public AlertDto findLastAlertBySensorId(UUID sensorId) {
-    // TODO: Task 5
-    return null;
-  }
+    public AlertDto findLastAlertBySensorId(UUID sensorId) {
+        return alertRepository.findLastOneBySensorId(sensorId)
+                .map(alert -> new AlertDto(alert.getSensor().getId(), alert.getMessage(), alert.getTimestamp()))
+                .orElseThrow(() -> new EntityNotFoundException(2001, String.format("No alert found for sensor with id %s.", sensorId)));
+    }
 }
