4a2cfbeb684a3c876c1e798b96a87ae918c1be1b
56839 <david.kun2@4ig.hu>
2025-10-27 15:08:38 +0100

    Task 7 solution

--- src/main/java/com/aldisued/iot/monitoring/service/MeasurementService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/MeasurementService.java
@@ -1,30 +1,35 @@
 package com.aldisued.iot.monitoring.service;
 
+import com.aldisued.iot.monitoring.entity.SensorReading;
 import com.aldisued.iot.monitoring.entity.SensorType;
 import com.aldisued.iot.monitoring.repository.SensorReadingRepository;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.stereotype.Service;
 
 @Service
 public class MeasurementService {
 
-  private final SensorReadingRepository sensorReadingRepository;
+    private final SensorReadingRepository sensorReadingRepository;
 
-  public MeasurementService(SensorReadingRepository sensorReadingRepository) {
-    this.sensorReadingRepository = sensorReadingRepository;
-  }
+    public MeasurementService(SensorReadingRepository sensorReadingRepository) {
+        this.sensorReadingRepository = sensorReadingRepository;
+    }
 
-  public List<Double> getMeasurementValuesBySensorType(SensorType sensorType, LocalDateTime from,
-      LocalDateTime to) {
-    // TODO: Task 8
-    return List.of();
-  }
+    public List<Double> getMeasurementValuesBySensorType(SensorType sensorType, LocalDateTime from, LocalDateTime to) {
+        // TODO: Task 8
+        return List.of();
+    }
 
-  public Optional<Double> getAverageTemperature(LocalDateTime from, LocalDateTime to) {
-    // TODO: Task 7
-    return Optional.empty();
-  }
+    public Optional<Double> getAverageTemperature(LocalDateTime from, LocalDateTime to) {
+        List<SensorReading> sensorReadings = sensorReadingRepository.findAllByTimestampIsBetweenAndSensorType(from, to, SensorType.TEMPERATURE);
+
+        return sensorReadings.stream()
+                .map(SensorReading::getValue)
+                .reduce(Double::sum)
+                .map(sum -> sum / sensorReadings.size());
+    }
 
 }
